73d77ffa68befa62fa7459e7ae745c0e98f33cb3
princexoleo <xtleon.xl@gmail.com>
2019-07-26 00:23:59 +0600

    products model added to master

--- products/models.py
+++ products/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    title       = models.CharField(max_length=50)
+    description = models.TextField()
+    price       = models.DecimalField(max_digits=10, decimal_places=2, default=10.99)
+
+    def __str__(self):
+        return self.title
+    
+
+
--- products/views.py
+++ products/views.py
@@ -1,3 +1,19 @@
+from django.views import ListView
 from django.shortcuts import render
 
+from .models import Product
+
 # Create your views here.
+# class based view 
+class ProductListView(ListView):
+    queryset = Product.objects.all()
+    
+
+
+#function based listview
+def product_list_view(request):
+    queryset = Product.objects.all()
+    context =[
+        'objects_list':queryset,
+    ]
+    return render(request,'product/product_list_view.html', context)
