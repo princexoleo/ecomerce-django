f6a21a1bb9428e6f32a9bcd6216137133179be38
princexoleo <xtleon.xl@gmail.com>
2019-07-25 15:07:44 +0600

    login register forms view added to master

--- ecomerce/urls.py
+++ ecomerce/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from ecomerce import views
@@ -22,4 +24,9 @@ urlpatterns = [
     path('',views.home_page),
     path('about/',views.about_page),
     path('contact/',views.contact_page),
+    path('login/',views.login_page),
+    path('register/',views.register_page),
 ]
+if settings.DEBUG:
+    urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- ecomerce/views.py
+++ ecomerce/views.py
@@ -1,12 +1,17 @@
+from django.contrib.auth import authenticate,login,get_user_model
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from ecomerce.forms import ContactForm, RegisterForm, LoginForm
 
 #create function here
 
 def home_page(request):
     context ={
         'title':'Home',
+        
     }
+    if request.user.is_authenticated:
+        context['primium_content'] = 'Primium Oh yeah ! primium content'
     return render(request, 'home/home_page.html', context)
 
 #about pages
@@ -18,10 +23,52 @@ def about_page(request):
 #
 # contact pages
 def contact_page(request):
+    contact_form = ContactForm(request.POST or None)
     context ={
         'title':'Contact',
+        'form': contact_form,
     }
-    if request.method == 'POST':
-        print(request.POST)
-        print(request.POST.get('name'))
+    if contact_form.is_valid():
+        print(contact_form.cleaned_data)
+    # if request.method == 'POST':
+    #     print(request.POST)
+    #     print(request.POST.get('fullname'))
     return render(request, 'contact/contact_page.html', context)
+
+def login_page(request):
+    form  = LoginForm(request.POST or None)
+    print(request.user.is_authenticated)
+    context ={
+        'title':'Login',
+        'form': form,
+    }
+    if form.is_valid():
+        print(form.cleaned_data)
+        username = form.cleaned_data.get('username')
+        row_password = form.cleaned_data.get('password')
+        user = authenticate(request, username = username, password = row_password)
+        print(request.user.is_authenticated)
+        if user is not None:
+            print(request.user.is_authenticated)
+            login(request, user)
+            #context['form'] = LoginForm()
+            print('Redirect Success Page')
+            return redirect('/')
+        else:
+            print('Login Error')
+    return render(request, 'auth/login_page.html', context)
+
+User = get_user_model()
+def register_page(request):
+    form  = RegisterForm(request.POST or None)
+    context ={
+        'title':'Register',
+        'form': form,
+    }
+    if form.is_valid():
+        print(form.cleaned_data)
+        username = form.cleaned_data.get('username')
+        email = form.cleaned_data.get('email')
+        row_password = form.cleaned_data.get('password')
+        User.objects.create_user()
+    return render(request, 'auth/register_page.html', context)
