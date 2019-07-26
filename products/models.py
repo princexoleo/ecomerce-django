94218a9a3a6bf0de5720a4f17485106112e9642c
princexoleo <xtleon.xl@gmail.com>
2019-07-26 20:32:34 +0600

    product image uploading done and added to master

--- products/models.py
+++ products/models.py
@@ -1,10 +1,33 @@
+import os
+import random
 from django.db import models
 
 # Create your models here.
+def get_filename_ext(filepath):
+    base_name   = os.path.basename(filepath)
+    name , ext = os.path.splitext(base_name)
+    return name,ext
+
+
+
+def upload_image_path(instance, filename):
+    #print(filename)
+    #print(instance)
+    new_filename = random.randint(1,10000)
+    name , ext  = get_filename_ext(filename)
+    final_filename = '{new_filename}{ext}'.format(new_filename=new_filename, ext=ext)
+    return "products/{new_filename}/{final_filename}".format(
+        new_filename=new_filename,
+         final_filename=final_filename
+         )
+
+
+
 class Product(models.Model):
     title       = models.CharField(max_length=50)
     description = models.TextField()
     price       = models.DecimalField(max_digits=10, decimal_places=2, default=10.99)
+    image       = models.ImageField(upload_to=upload_image_path,null=True, blank=True)
 
     def __str__(self):
         return self.title
